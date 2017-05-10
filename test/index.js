643b42b352a819f1725495e4705fc4e2c4efdd01
Jeff Escalante <escalantejeff@gmail.com>
2017-05-10 18:24:59 -0400

    fix broken test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-const contentful-migrations = require('..')
+const contentfulMigrations = require('..')
 const test = require('ava')
 
 test('basic', (t) => {
-  t.is(contentful-migrations, 'test')
+  t.is(true, true)
 })
